d0764bb28f18f5b7a87b28f896cc5dd70b54045d
haeunNoh06 <okhagrace06@naver.com>
2023-12-06 21:24:43 +0900

    #18 :sparkles: [feat] 편지 writer post로 저장 완료

--- js/write-rolling-paper-creating.js
+++ js/write-rolling-paper-creating.js
@@ -1,4 +1,5 @@
 let text = document.getElementById('text');
+let sender = document.getElementById('sender_input');
 let normalBtn = document.getElementsByClassName('font-change')[0];
 let boldBtn = document.getElementsByClassName('font_change')[0];
 let italicBtn = document.getElementsByClassName('font_change')[0];
@@ -13,4 +14,20 @@ function changeFont(font){
 function findFont(){
     console.log(currentFont , text.value);
     return currentFont;
+}
+
+function showListMemo(){
+    var memo = document.createElement('div');
+    var fromSender = document.createElement('div');
+    
+    var text = text.value;
+    var senderName = sender.value;
+    memo.innerText= text;
+    fromSender.innerText = `From. ${senderName}`;
+    memo.className = 'memo';
+    fromSender.className = 'from_sender';
+    
+    console.log(memo);
+    paper.appendChild(memo);
+    memo.appendChild(fromSender);
 }
--- js/write-rolling-paper.js
+++ js/write-rolling-paper.js
@@ -9,16 +9,18 @@ let memoboxList = [];
 // var text = 'test';
 // var senderName = 'testUser';
 
-function showListMemo(){
-    var memo = document.createElement('div');
-    var fromSender = document.createElement('div');
+// function showListMemo(){
+//     var memo = document.createElement('div');
+//     var fromSender = document.createElement('div');
     
-    memo.innerText= text;
-    fromSender.innerText = `From. ${senderName}`;
-    memo.className = 'memo';
-    fromSender.className = 'from_sender';
+//     var text = 'test';
+//     var senderName = 'testUser';
+//     memo.innerText= text;
+//     fromSender.innerText = `From. ${senderName}`;
+//     memo.className = 'memo';
+//     fromSender.className = 'from_sender';
     
-    console.log(memo);
-    paper.appendChild(memo);
-    memo.appendChild(fromSender);
-}
+//     console.log(memo);
+//     paper.appendChild(memo);
+//     memo.appendChild(fromSender);
+// }
