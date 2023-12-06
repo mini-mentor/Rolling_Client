5afe7f24024eaf1455d9d732467325b82bc2d0e4
rlagkdmss <kimhaeun060327@gmail.com>
2023-12-06 20:28:04 +0900

    [design] 롤링페이퍼 쪽지 작성 화면 보내는 사람  및 롤링페이퍼 화면 보낸 사람 디자인 추가

--- js/write-rolling-paper.js
+++ js/write-rolling-paper.js
@@ -3,13 +3,22 @@ let createBtn = document.getElementsByClassName('edit_btn')[0];
 let paper = document.getElementsByClassName('middle_box')[0];
 
 let memoboxList = [];
-for(var x of memoboxList){
-    createListMemo(x);
-}
-async function createListMemo(){
+// for(var x of memoboxList){
+//     createListMemo(x);
+// }
+// var text = 'test';
+// var senderName = 'testUser';
+
+function showListMemo(){
     var memo = document.createElement('div');
+    var fromSender = document.createElement('div');
+    
     memo.innerText= text;
+    fromSender.innerText = `From. ${senderName}`;
     memo.className = 'memo';
+    fromSender.className = 'from_sender';
+    
     console.log(memo);
     paper.appendChild(memo);
+    memo.appendChild(fromSender);
 }
