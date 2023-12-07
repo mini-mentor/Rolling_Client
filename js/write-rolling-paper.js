231198d85257830ed99281f0669b27d066005c34
rlagkdmss <kimhaeun060327@gmail.com>
2023-12-07 09:26:09 +0900

    ✨[feat] 쪽지 내욤 일정 길이 이상의 글자 대체, 쪽지 누르면 쪽지 전체 내용이 보여지는 화면으로 이동

--- js/write-rolling-paper.js
+++ js/write-rolling-paper.js
@@ -2,25 +2,46 @@ let contentBox = document.getElementsByClassName('middle_box')[0];
 let createBtn = document.getElementsByClassName('edit_btn')[0];
 let paper = document.getElementsByClassName('middle_box')[0];
 
-let memoboxList = [];
-// for(var x of memoboxList){
-//     createListMemo(x);
-// }
-// var text = 'test';
-// var senderName = 'testUser';
+const shortenWords = (str, length = 30) => {
+    let result = '';
+    if(str.length > length){
+        result = str.substr(0, length-2)+'...';
+    }else {
+        result = str;
+    }
+    return result;
+}
 
-// function showListMemo(){
-//     var memo = document.createElement('div');
-//     var fromSender = document.createElement('div');
+function showListMemo(){
+    var memo = document.createElement('div');
+    var fromSender = document.createElement('div');
     
-//     var text = text.value;
-//     var senderName = sender.value;
-//     memo.innerText= text;
-//     fromSender.innerText = `From. ${senderName}`;
-//     memo.className = 'memo';
-//     fromSender.className = 'from_sender';
+    var text = 'text.valuefkajfd;alkfjalsfafdk;afjdslf;sakldfjadssafdadfdfsl';
     
-//     console.log(memo);
-//     paper.appendChild(memo);
-//     memo.appendChild(fromSender);
-// }
+    text = shortenWords(text);
+    var senderName = 'sender.value';
+    senderName = shortenWords(senderName, 6)
+    memo.innerText= text;
+    fromSender.innerText = `From. ${senderName}`;
+    memo.className = 'memo';
+    fromSender.className = 'from_sender';
+
+    
+    // console.log(memo);
+    paper.appendChild(memo);
+    memo.appendChild(fromSender);
+
+linked();
+    
+    
+
+}
+
+function linked(){
+var memolist = document.getElementsByClassName('memo');
+    console.log(memolist);
+  Array.from(memolist).forEach(e => {
+    console.log(e);
+    e.addEventListener('click', ()=>{location.href = '\..\/write-rolling-paper-viewing.html'})
+});
+}
